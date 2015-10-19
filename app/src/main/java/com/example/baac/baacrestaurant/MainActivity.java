e6e8d1cf33ca870da95be419d099106f8996e7f1
NINGE <supattar@baac.or.th>
2015-10-19 15:59:30 +0700

    Finish Day 1

--- app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
+++ app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
@@ -5,9 +5,33 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private UserTABLE objUserTABLE;
+    private FoodTABLE objFoodTABLE;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Create & Connect Database
+        createAndConnect();
+
+        //Test Add New Value
+        testAdd();
+
+    } //Main Method
+
+    private void testAdd() {
+        objUserTABLE.addNewUser("testUser","testPassword","testทดสอบชื่อภาษาไทย");
+        objFoodTABLE.addNewFood("testFood","testSource","testPrice");
     }
-}
+
+    private void createAndConnect() {
+        objUserTABLE = new UserTABLE(this);
+        objFoodTABLE = new FoodTABLE(this);
+    }
+
+} //Main Class
