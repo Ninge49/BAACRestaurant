e24ff0995f80c19e07b96aee6ff8c77167280a6a
NINGE <supattar@baac.or.th>
2015-10-19 12:54:48 +0700

    Start Project

---
+++ app/src/main/java/com/example/baac/baacrestaurant/MyOpenHelper.java
@@ -0,0 +1,23 @@
+package com.example.baac.baacrestaurant;
+
+import android.content.Context;
+
+/**
+ * Created by BAAC on 10/19/2015.
+ */
+public class MyOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "BAAC.db";
+    private static final Integer DATABASE_VERSION = 1;
+
+
+
+
+
+
+
+    public MyOpenHelper(Context context) {
+    } //Constructor
+
+} //Main Class
