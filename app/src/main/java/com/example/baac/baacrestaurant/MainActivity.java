eb0d6320f560e887e91b5113b34b00385883bb9f
NINGE <supattar@baac.or.th>
2015-10-20 09:46:58 +0700

    Delete All SQLite Success

--- app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
+++ app/src/main/java/com/example/baac/baacrestaurant/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.baac.baacrestaurant;
 
+import android.database.sqlite.SQLiteAbortException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -20,9 +22,18 @@ public class MainActivity extends AppCompatActivity {
         createAndConnect();
 
         //Test Add New Value
-        testAdd();
+        //testAdd();
+
+        //Delete Database
+        deleteAllSQLite();
 
     } //Main Method
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("BAAC.db", MODE_PRIVATE, null);
+        objSqLiteDatabase.delete("userTABLE",null,null);
+        objSqLiteDatabase.delete("foodTABLE",null,null);
+    }
 
     private void testAdd() {
         objUserTABLE.addNewUser("testUser","testPassword","testทดสอบชื่อภาษาไทย");
